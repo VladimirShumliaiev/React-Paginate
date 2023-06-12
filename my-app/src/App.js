10db2d0cd3e7cb135693f3ef067c54597551a0ae
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-06-12 15:54:41 +0300

    add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
+import Users from "./Components/Users";
+import axios from "axios";
 
 function App() {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+      axios.get('https://jsonplaceholder.typicode.com/users').then
+      (({data}) => setUsers(data))
+  },[])
   return (
     <div className="App">
-
+      <Users data={users}/>
     </div>
   );
 }
---
+++ my-app/src/Components/Users.jsx
@@ -0,0 +1,61 @@
+import React, {useEffect, useState} from 'react';
+import style from './Users.module.css'
+import ReactPaginate from "react-paginate";
+
+const Users = (props) => {
+    const {data} = props
+    const [currentItems, setCurrentItems] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [itemOffset, setItemOffset] = useState(0);
+    const itemsPerPage = 1
+
+    useEffect(() => {
+        const endOffset = itemOffset + itemsPerPage;
+        console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+        setCurrentItems(data.slice(itemOffset, endOffset));
+        setPageCount(Math.ceil(data.length / itemsPerPage));
+    }, [itemOffset, itemsPerPage, data]);
+
+    const handlePageClick = (event) => {
+        const newOffset = event.selected * itemsPerPage % data.length;
+        console.log(`User requested page number ${event.selected}, which is offset ${newOffset}`);
+        setItemOffset(newOffset);
+    };
+
+    return (
+        <>
+            <div>
+                {currentItems.map(user => {
+                    return (
+                        <div key={user.id}>
+                            <div>
+                                {user.name}
+                            </div>
+                            <div>
+                                {user.username}
+                            </div>
+                            <hr/>
+                        </div>
+                    )
+                })}
+            </div>
+            <ReactPaginate
+                breakLabel="..."
+                nextLabel="next >"
+                onPageChange={handlePageClick}
+                pageRangeDisplayed={5}
+                pageCount={pageCount}
+                previousLabel="< previous"
+                renderOnZeroPageCount={null}
+                containerClassName={style.Pagination}
+                pageLinkClassName={style.pageNum}
+                previousLinkClassName={style.pageNum}
+                nextLinkClassName={style.pageNum}
+                activeLinkClassName={style.active}
+
+            />
+        </>
+    );
+};
+
+export default Users;
